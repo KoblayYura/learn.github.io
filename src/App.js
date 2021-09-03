91139502f450dfc22478b9d311b100110b31c6de
KoblayYura <koblay.yuriy@gmail.com>
2021-09-03 23:17:08 +0300

    123

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  (async ()=> {
+    await fetch('https://api.gismeteo.net/v2/weather/current/?latitude=54.35&longitude=52.52',{
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      // mode: 'no-cors', // no-cors, *cors, same-origin
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Gismeteo-Token': '56b30cb255.3443075',
+        'Accept-Encoding': 'deflate',
+        'Access-Control-Allow-Origin': 'http://localhost:3000'
+      },
+    }).then(res => console.log(res))
+  })()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
